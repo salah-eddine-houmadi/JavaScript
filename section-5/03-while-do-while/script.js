0fe0a023beb8b685b95cc112e4016cd569cf0ed8
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-26 19:34:49 +0100

    while-do-while

---
+++ section-5/03-while-do-while/script.js
@@ -0,0 +1,30 @@
+let i = 0;
+
+while (i <= 20) {
+  console.log("Number " + i);
+  i++;
+}
+
+const arr = [10, 20, 30, 40, 50];
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
+
+while (i <= 5) {
+  console.log("Number " + i);
+
+  let j = 1;
+  while (j <= 5) {
+    console.log(`${i} * ${j} = ${i * j}`);
+    j++;
+  }
+
+  i++;
+}
+
+do {
+  console.log("Number " + i);
+  i++;
+} while (i <= 20);
