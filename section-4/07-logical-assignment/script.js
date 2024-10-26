aa4e097d0317a14123c2c6c6dbc4d7069ba65095
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-26 22:29:44 +0200

    Logical assignment (#25)

---
+++ section-4/07-logical-assignment/script.js
@@ -0,0 +1,29 @@
+let a = null;
+
+a ||= 10;
+
+console.log(a);
+
+let b = 10;
+
+if (b) {
+  b = 20;
+}
+
+b = b && 20;
+
+b &&= 20;
+
+console.log(b);
+
+let c = null;
+
+if (c === null || c === undefined) {
+  c = 20;
+}
+
+c = c ?? 20;
+
+c ??= 20;
+
+console.log(c);
