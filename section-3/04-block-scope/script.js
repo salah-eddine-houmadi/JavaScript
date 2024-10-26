7723fc9669714f4419e7bf5cbef7ae2528010315
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-26 16:26:38 +0200

    Block scope (#11)

---
+++ section-3/04-block-scope/script.js
@@ -0,0 +1,35 @@
+const x = 100;
+
+
+if (true) {
+  console.log(x);
+  const y = 200;
+  console.log(x + y);
+}
+
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+console.log(c);
+
+
+function run() {
+  var d = 100;
+  console.log(d);
+}
+
+run();
+
+
+
+const foo = 1;
+var bar = 2; 
