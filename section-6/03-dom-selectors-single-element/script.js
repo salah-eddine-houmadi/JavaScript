72da6dd0be6b38f9d6c4299aaa1e7aff31a02309
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-27 18:41:16 +0100

    Dom selectors single element (#40)

---
+++ section-6/03-dom-selectors-single-element/script.js
@@ -0,0 +1,35 @@
+console.log(document.getElementById("app-title"));
+
+console.log(document.getElementById("app-title").id);
+console.log(document.getElementById("app-title").className);
+console.log(document.getElementById("app-title").getAttribute("id"));
+
+document.getElementById("app-title").title = "Shopping List";
+document.getElementById("app-title").setAttribute("class", "title");
+
+const title = document.getElementById("app-title");
+
+console.log(title.textContent);
+title.textContent = "Hello World";
+title.innerText = "Hello Again";
+title.innerHTML = "<strong>Shopping List</strong>";
+
+title.style.color = "red";
+title.style.backgroundColor = "black";
+title.style.padding = "10px";
+title.style.borderRadius = "10px";
+
+console.log(document.querySelector("h1"));
+console.log(document.querySelector("#app-title"));
+console.log(document.querySelector(".container"));
+console.log(document.querySelector('input[type="text"]'));
+console.log(document.querySelector("li:nth-child(2)").innerText);
+
+const secondItem = document.querySelector("li:nth-child(2)");
+secondItem.innerText = "Apple Juice";
+secondItem.style.color = "red";
+
+const list = document.querySelector("ul");
+console.log(list);
+const firstItem = list.querySelector("li");
+firstItem.style.color = "blue";
