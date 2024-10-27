9edf9e05f4e51b6fd9e2467f2b11fed7fc6dd586
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-27 17:03:05 +0100

    insert-elements

---
+++ section-6/10-insert-elements/script.js
@@ -0,0 +1,33 @@
+function insertElement() {
+  const filter = document.querySelector(".filter");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "insertAdjacentElement";
+
+  filter.insertAdjacentElement("beforebegin", h1);
+}
+
+function insertText() {
+  const item = document.querySelector("li:first-child");
+
+  item.insertAdjacentText("beforebegin", "insertAdjacentText");
+}
+
+function insertHTML() {
+  const clearBtn = document.querySelector("#clear");
+
+  clearBtn.insertAdjacentHTML("afterend", "<h2>insertAdjacentHTML</h2>");
+}
+
+function insertBeforeItem() {
+  const ul = document.querySelector("ul");
+
+  const li = document.createElement("li");
+  li.textContent = "insertBefore";
+
+  const thirdItem = document.querySelector("li:nth-child(3)");
+
+  ul.insertBefore(li, thirdItem);
+}
+
+insertElement();
