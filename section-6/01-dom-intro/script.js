1a83287a613d1b02d928dcd1c534c745f6b97638
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-27 18:39:21 +0100

    dom-into (#38)

---
+++ section-6/01-dom-intro/script.js
@@ -0,0 +1,12 @@
+console.log(window);
+
+console.dir(window.document);
+
+console.log(document.body);
+console.log(document.links[0]);
+
+document.write("Hello from JS");
+
+document.getElementById("main").innerHTML = "<h1>Hello from main</h1>";
+
+document.querySelector("#main h1").innerText = "Hello";
