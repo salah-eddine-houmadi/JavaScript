aa53f96655d789801ff439a7500d1500907f3ca9
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-27 17:13:12 +0100

    custom-insertafter-challenge

---
+++ section-6/11-custom-insertafter-challenge/script.js
@@ -0,0 +1,10 @@
+function insertAfter(newEl, existingEl) {
+  existingEl.parentElement.insertBefore(newEl, existingEl.nextSibling);
+}
+
+const li = document.createElement("li");
+li.textContent = "Insert Me After!";
+
+const firstItem = document.querySelector("li:first-child");
+
+insertAfter(li, firstItem);
