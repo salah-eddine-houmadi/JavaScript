2cbd3da71685b2024c42cd5e498b066b46885457
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-10-29 20:26:24 +0100

    Event delegation (#59)

---
+++ section-7/09-event-delegation/script.js
@@ -0,0 +1,21 @@
+const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+
+// Add an event listener on all items
+// listItems.forEach((item) => {
+//   item.addEventListener('click', (e) => {
+//     e.target.remove();
+//   });
+// });
+
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.remove();
+  }
+});
+
+list.addEventListener("mouseover", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.style.color = "red";
+  }
+});
