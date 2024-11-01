4b576bb5e80ce6b8cbb08f0dc03038c42d7174ad
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-11-01 15:29:27 +0100

    Add prototype methods (#106)

---
+++ section-13/05-prototypes/script.js
@@ -0,0 +1,14 @@
+function Rectangle(name, width, height) {
+  this.name = name;
+  this.width = width;
+  this.height = height;
+  this.area = function () {
+    return this.width * this.height;
+  };
+}
+
+const rect = new Rectangle("Rect", 10, 10);
+
+console.log(rect.toString());
+
+console.log(Object.getPrototypeOf(rect));
