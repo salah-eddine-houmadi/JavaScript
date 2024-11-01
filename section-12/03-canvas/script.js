833db836c241665411c596ab3d27568e785f693e
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-11-01 15:21:52 +0100

    Canvas (#90)

---
+++ section-12/03-canvas/script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById("my-canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
+
+ctx.fillStyle = "red";
+ctx.arc(300, 300, 100, 0, Math.PI * 2);
+ctx.fill();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.moveTo(10, 10);
+ctx.lineTo(300, 300);
+ctx.stroke();
+
+ctx.font = "30px Arial";
+ctx.lineWidth = 1;
+ctx.fillStyle = "blue";
+ctx.strokeStyle = "blue";
+ctx.fillText("Hello World", 300, 100, 300);
+ctx.strokeText("Hello World", 300, 500, 300);
+
+const image = document.querySelector("img");
+image.style.display = "none";
+
+image.addEventListener("load", () => ctx.drawImage(image, 250, 250, 100, 100));
