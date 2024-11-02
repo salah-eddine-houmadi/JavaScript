61182c2b1098718955fbd3180fdd05ff2f85cbff
salah eddine houmadi <79973147+salah-eddine-houmadi@users.noreply.github.com>
2024-11-02 18:24:44 +0100

    randomldes-app-webpack-frontend

---
+++ section-21/randomldes-app-webpack-frontend/server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the RandomIdeas API' });
+});
+
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
